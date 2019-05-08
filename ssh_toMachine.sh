83a5cbcdf543aaa21f7c661e7431e68a7605b1d0
Rogerio Tadim <rogeriotadim@gmail.com>
2019-05-08 15:39:37 -0300

    ssh to machine

---
+++ ssh_toMachine.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker_machine_name=$1
+docker_ssh_user=$(docker-machine inspect $docker_machine_name --format={{.Driver.SSHUser}})
+docker_ssh_key=$(docker-machine inspect $docker_machine_name --format={{.Driver.SSHKeyPath}})
+docker_ssh_port=$(docker-machine inspect $docker_machine_name --format={{.Driver.SSHPort}})
+
+ssh -i $docker_ssh_key -p $docker_ssh_port $docker_ssh_user@localhost  
+
