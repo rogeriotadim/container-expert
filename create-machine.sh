c80e790b507a4154617167a986d09a5a7f3b43a2
Rogerio Tadim <rogeriotadim@gmail.com>
2019-05-09 17:35:30 -0300

    ajuste scripts

--- create-machine.sh
+++ create-machine.sh
@@ -1,8 +1,11 @@
-#! /bin/bash
+#!/bin/bash
+export http_proxy=http://10.16.5.42:3128
+export https_proxy=http://10.16.5.42:3128
 
-docker-machine create  --driver virtualbox --virtualbox-disk-size "10000" --virtualbox-cpu-count "1" --virtualbox-memory "1024" $MN
-### --engine-env  http_proxy=http://10.16.5.42:3128 --engine-env https_proxy=http://10.16.5.42:3128
+docker-machine create \
+   --engine-env http_proxy=http://10.16.5.42:3128 --engine-env https_proxy=http://10.16.5.42:3128 \
+   --driver virtualbox --virtualbox-disk-size "10000" --virtualbox-cpu-count "1" --virtualbox-memory "1024" $MN
 
-docker-machine env $MN >./$MN.env
+docker-machine env $MN >./$MN.environment
 
 eval "$(docker-machine env $MN)"
