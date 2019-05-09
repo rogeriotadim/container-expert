d1d65115d8e5ddb6e2f515def41f926dd537b2b4
Rogerio Tadim <rogeriotadim@gmail.com>
2019-05-09 11:00:45 -0300

    short machine name

--- create-machine.sh
+++ create-machine.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
-export MACHINE_NAME=$1
-
-docker-machine create  --driver virtualbox --virtualbox-disk-size "10000" --virtualbox-cpu-count "1" --virtualbox-memory "1024" $MACHINE_NAME
+docker-machine create  --driver virtualbox --virtualbox-disk-size "10000" --virtualbox-cpu-count "1" --virtualbox-memory "1024" $MN
 ### --engine-env  http_proxy=http://10.16.5.42:3128 --engine-env https_proxy=http://10.16.5.42:3128
 
-docker-machine env $MACHINE_NAME >./$MACHINE_NAME.env
+docker-machine env $MN >./$MN.env
 
-eval "$(docker-machine env $MACHINE_NAME)"
+eval "$(docker-machine env $MN)"
