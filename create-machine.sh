3d258974db9e5236fba0bc88e2df7f91e4ae6901
Rogerio Tadim <rogerio.tadim@gmail.com>
2019-05-09 01:07:43 -0300

    evolution

--- create-machine.sh
+++ create-machine.sh
@@ -2,6 +2,9 @@
 
 export MACHINE_NAME=$1
 
-docker-machine create --engine-env  http_proxy=http://10.16.5.42:3128 --engine-env https_proxy=http://10.16.5.42:3128 --driver virtualbox --virtualbox-disk-size "10000" $MACHINE_NAME
+docker-machine create  --driver virtualbox --virtualbox-disk-size "10000" --virtualbox-cpu-count "1" --virtualbox-memory "1024" $MACHINE_NAME
+### --engine-env  http_proxy=http://10.16.5.42:3128 --engine-env https_proxy=http://10.16.5.42:3128
 
-docker-machine.exe env $MACHINE_NAME >./$MACHINE_NAME.env
+docker-machine env $MACHINE_NAME >./$MACHINE_NAME.env
+
+eval "$(docker-machine env $MACHINE_NAME)"
