99b537b699e966ee4ae29ed031b9ad79d0a8f34a
rogeriotadim <rogeriotadim@users.noreply.github.com>
2019-06-11 23:31:08 -0300

    correcao 01

--- gcp/01-apt-update.sh
+++ gcp/01-apt-update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-apt update && apt upgrade -y
-apt install -y git nano
-git confing --global user.name "Rogerio Tadim"
+apt update && apt upgrade -y && apt install -y git nano apt-utils 
+git config --global user.name "Rogerio Tadim"
 git config --global user.email "rogeriotadim@gmail.com"
