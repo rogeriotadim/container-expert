504c6b9bec3af8dcc42328e33d8f66290ae382b6
rogeriotadim <rogeriotadim@users.noreply.github.com>
2019-06-14 13:43:02 -0300

    junção dos comandos

--- gcp/01-apt-update.sh
+++ gcp/01-apt-update.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-apt update && apt upgrade -y && apt install -y git nano apt-utils 
-git config --global user.name "Rogerio Tadim"
-git config --global user.email "rogeriotadim@gmail.com"
+apt update && apt upgrade -y && apt install -y git nano apt-utils && git config --global user.name "Rogerio Tadim" && git config --global user.email "rogeriotadim@gmail.com"
